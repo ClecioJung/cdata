c246fc013d408d80cf22944b7bf87902dc6ddb7b
Clecio Jung <clecio.jung@gmail.com>
2024-02-23 23:49:45 -0300

    Adding command line arguments for the example

--- README.md
+++ README.md
@@ -1,121 +1,104 @@
 # cdata
 
-Header Only Libraries for C data-structures and some examples.
+Header Only Library for C data-structures and some examples.
 
 ## Usage
 
 ```console
 $ make
-mkdir Release
-gcc -pedantic -Wall -Wextra -Werror -std=c99 -O2 -flto main.c -o Release/count
+gcc -pedantic -W -Wall -Wextra -Wconversion -Wswitch-enum -Werror -std=c99 -O0 -g -I.  examples/count-words.c -o examples/count-words
 
-$ ./Release/count The\ Divine\ Comedy.txt Shakespeare.txt 
-File: The Divine Comedy.txt
+$ ./examples/count-words examples/The\ Divine\ Comedy.txt examples/Shakespeare.txt 
+File: examples/The Divine Comedy.txt
+  lines: 6390
+  chars: 220001
+  words: 38039
   algorithm: dynamic array
-  execution time: 0.895444s
-  lines: 6390
-  chars: 220001
-  words: 38039
-  unique words: 8452
-  top 10 words:
-    01. the               1943
-    02. and                896
-    03. I                  779
-    04. of                 751
-    05. to                 723
-    06. And                587
-    07. that               483
-    08. a                  389
-    09. in                 376
-    10. he                 376
-File: The Divine Comedy.txt
+    execution time: 1.78038s
+    unique words: 7783
+    top 10 words:
+      01. the               2213
+      02. and               1483
+      03. of                 854
+      04. to                 806
+      05. i                  779
+      06. that               651
+      07. he                 450
+      08. in                 441
+      09. a                  436
+      10. with               397
   algorithm: sorted dynamic array
-  execution time: 0.020293s
-  lines: 6390
-  chars: 220001
-  words: 38039
-  unique words: 8452
-  top 10 words:
-    01. the               1943
-    02. and                896
-    03. I                  779
-    04. of                 751
-    05. to                 723
-    06. And                587
-    07. that               483
-    08. a                  389
-    09. he                 376
-    10. in                 376
-File: The Divine Comedy.txt
+    execution time: 0.039635s
+    unique words: 7783
+    top 10 words:
+      01. the               2213
+      02. and               1483
+      03. of                 854
+      04. to                 806
+      05. i                  779
+      06. that               651
+      07. he                 450
+      08. in                 441
+      09. a                  436
+      10. with               397
   algorithm: hash table
-  execution time: 0.009593s
-  lines: 6390
-  chars: 220001
-  words: 38039
-  unique words: 8452
-  top 10 words:
-    01. the               1943
-    02. and                896
-    03. I                  779
-    04. of                 751
-    05. to                 723
-    06. And                587
-    07. that               483
-    08. a                  389
-    09. he                 376
-    10. in                 376
-File: Shakespeare.txt
+    execution time: 0.020831s
+    unique words: 7783
+    top 10 words:
+      01. the               2213
+      02. and               1483
+      03. of                 854
+      04. to                 806
+      05. i                  779
+      06. that               651
+      07. he                 450
+      08. in                 441
+      09. a                  436
+      10. with               397
+File: examples/Shakespeare.txt
+  lines: 124455
+  chars: 5458198
+  words: 900987
   algorithm: dynamic array
-  execution time: 102.059s
-  lines: 124455
-  chars: 5458198
-  words: 900987
-  unique words: 66597
-  top 10 words:
-    01. the              23255
-    02. I                19593
-    03. and              18312
-    04. to               15638
-    05. of               15545
-    06. a                12648
-    07. my               10828
-    08. in                9581
-    09. you               9088
-    10. is                7853
-File: Shakespeare.txt
+    execution time: 188.265s
+    unique words: 58580
+    top 10 words:
+      01. the              27627
+      02. and              26063
+      03. i                19593
+      04. to               18996
+      05. of               18011
+      06. a                14589
+      07. my               12476
+      08. in               10682
+      09. you              10642
+      10. that             10498
   algorithm: sorted dynamic array
-  execution time: 0.924233s
-  lines: 124455
-  chars: 5458198
-  words: 900987
-  unique words: 66597
-  top 10 words:
-    01. the              23255
-    02. I                19593
-    03. and              18312
-    04. to               15638
-    05. of               15545
-    06. a                12648
-    07. my               10828
-    08. in                9581
-    09. you               9088
-    10. is                7853
-File: Shakespeare.txt
+    execution time: 1.30934s
+    unique words: 58580
+    top 10 words:
+      01. the              27627
+      02. and              26063
+      03. i                19593
+      04. to               18996
+      05. of               18011
+      06. a                14589
+      07. my               12476
+      08. in               10682
+      09. you              10642
+      10. that             10498
   algorithm: hash table
-  execution time: 0.184709s
-  lines: 124455
-  chars: 5458198
-  words: 900987
-  unique words: 66597
-  top 10 words:
-    01. the              23255
-    02. I                19593
-    03. and              18312
-    04. to               15638
-    05. of               15545
-    06. a                12648
-    07. my               10828
-    08. in                9581
-    09. you               9088
-    10. is                7853
+    execution time: 0.462562s
+    unique words: 58580
+    top 10 words:
+      01. the              27627
+      02. and              26063
+      03. i                19593
+      04. to               18996
+      05. of               18011
+      06. a                14589
+      07. my               12476
+      08. in               10682
+      09. you              10642
+      10. that             10498
 ```
--- examples/count-words.c
+++ examples/count-words.c
@@ -24,6 +24,8 @@ typedef void (*Deinit_Fcn)(Word *);
 
 typedef struct {
     char *name;
+    char arg_option; // Command line argument to activate this algorithm
+    char *help_msg;
     Init_Fcn init;
     Process_Word_Fcn process_word;
     Post_Process_Fcn post_process;
@@ -87,12 +89,12 @@ void array_deinit(Word *array) {
 }
 
 void array_display_results(Word *const array, size_t number_of_words) {
-    printf("  unique words: %zu\n", array_size(array));
+    printf("    unique words: %zu\n", array_size(array));
     number_of_words = INT_MIN(number_of_words, array_size(array));
     if (number_of_words > 0) {
-        printf("  top %zu words:\n", number_of_words);
+        printf("    top %zu words:\n", number_of_words);
         for (size_t i = 0; i < number_of_words; i++) {
-            printf("    %02zu. %-15s %6zu\n", (i+1), array[i].word, array[i].count);
+            printf("      %02zu. %-15s %6zu\n", (i+1), array[i].word, array[i].count);
         }
     }
 }
@@ -140,6 +142,8 @@ Word *hash_algorithm(Word *hash_table, const Word word) {
 static const Algorithm algorithms[] = {
     {
         .name = "dynamic array",
+        .arg_option = 'd',
+        .help_msg = "Uses dynamic array with sequential search algorithm",
         .init = array_init,
         .process_word = sequential_algorithm,
         .post_process = array_sort_words_descending_by_count,
@@ -148,6 +152,8 @@ static const Algorithm algorithms[] = {
     },
     {
         .name = "sorted dynamic array",
+        .arg_option = 's',
+        .help_msg = "Uses sorted dynamic array algorithm",
         .init = array_init,
         .process_word = sorted_algorithm,
         .post_process = array_sort_words_descending_by_count,
@@ -156,6 +162,8 @@ static const Algorithm algorithms[] = {
     },
     {
         .name = "hash table",
+        .arg_option = 't',
+        .help_msg = "Uses hash table algorithm",
         .init = hash_table_init,
         .process_word = hash_algorithm,
         .post_process = convert_hash_table_to_sorted_array,
@@ -164,11 +172,18 @@ static const Algorithm algorithms[] = {
     },
 };
 
-int process_file(const char *const filename, const Algorithm algorithm, size_t number_of_words) {
+void copy_sized_str_to_cstr_lowercase(char *dst, const char *src, size_t len) {
+    for (size_t i = 0; (src[i] != '\0') && (i < len); i++) {
+        dst[i] = (char)tolower(src[i]);
+    }
+    dst[len] = '\0';
+}
+
+int process_file(const char *const filename, const Algorithm algorithm, int print_header, size_t number_of_words) {
     clock_t tic = clock();
     FILE *const file = fopen(filename, "rb");
     if (file == NULL) {
-        fprintf(stderr, "Could not open file %s: %s\n", filename, strerror(errno));
+        fprintf(stderr, "Error: Could not open file \"%s\": %s\n", filename, strerror(errno));
         return EXIT_FAILURE;
     }
     char tmp_buffer[4096];
@@ -188,10 +203,8 @@ int process_file(const char *const filename, const Algorithm algorithm, size_t n
             }
             if (word_len > 0) {
                 words++;
-                // TODO: convert all words to lowercase
                 assert(word_len < sizeof(tmp_buffer));
-                strncpy(tmp_buffer, str, word_len);
-                tmp_buffer[word_len] = '\0';
+                copy_sized_str_to_cstr_lowercase(tmp_buffer, str, word_len);
                 Word word_found = { .word = tmp_buffer };
                 data = algorithm.process_word(data, word_found);
             }
@@ -201,18 +214,20 @@ int process_file(const char *const filename, const Algorithm algorithm, size_t n
     }
     int result = EXIT_SUCCESS;
     if (!feof(file)) {
-        fprintf(stderr, "Didn't reach the end of file for %s\n", filename);
+        fprintf(stderr, "Error: Didn't reach the end of file for file \"%s\"\n", filename);
         result = EXIT_FAILURE;
     }
     data = algorithm.post_process(data);
     clock_t toc = clock();
     if (result == EXIT_SUCCESS) {
-        printf("File: %s\n", filename);
+        if (print_header) {
+            printf("File: %s\n", filename);
+            printf("  lines: %zu\n", lines);
+            printf("  chars: %zu\n", chars);
+            printf("  words: %zu\n", words);
+        }
         printf("  algorithm: %s\n", algorithm.name);
-        printf("  execution time: %gs\n", (double)(toc - tic) / CLOCKS_PER_SEC);
-        printf("  lines: %zu\n", lines);
-        printf("  chars: %zu\n", chars);
-        printf("  words: %zu\n", words);
+        printf("    execution time: %gs\n", (double)(toc - tic) / CLOCKS_PER_SEC);
         algorithm.display_results(data, number_of_words);
     }
     algorithm.deinit(data);
@@ -220,8 +235,34 @@ int process_file(const char *const filename, const Algorithm algorithm, size_t n
     return result;
 }
 
+int parse_uint(const char *const str, size_t *const number) {
+    char *endptr = NULL;
+    unsigned long parsed_number = strtoul(str, &endptr, 10);
+    if (*endptr != '\0') {
+        return EXIT_FAILURE;
+    }
+    *number = (size_t)parsed_number;
+    return EXIT_SUCCESS;
+}
+
+size_t algorithm_option(char arg) {
+    for (size_t i = 0; i < STATIC_ARRAY_LEN(algorithms); i++) {
+        if (arg == algorithms[i].arg_option) {
+            return i;
+        }
+    }
+    return (size_t)-1;
+}
+
 void usage(FILE *const stream, const char *const  program_name) {
-    fprintf(stream, "Usage: %s [file]\n", program_name);
+    fprintf(stream, "Usage: %s [options] file...\n", program_name);
+    fprintf(stream, "Options:\n");
+    for (size_t i = 0; i < STATIC_ARRAY_LEN(algorithms); i++) {
+        const Algorithm algorithm = algorithms[i];
+        fprintf(stream, "  -%c                       %s\n", algorithm.arg_option, algorithm.help_msg);
+    }
+    fprintf(stream, "  -n  <unsigned integer>   Specifies the number of most used words to display\n");
+    fprintf(stream, "  -h                       Display this help message\n");
 }
 
 int main(const int argc, const char *const argv[])
@@ -229,18 +270,79 @@ int main(const int argc, const char *const argv[])
     assert(argc > 0);
     const char *const program_name = argv[0];
     if (argc == 1) {
+        fprintf(stderr, "Error: no argument was provided!\n");
         usage(stderr, program_name);
         return EXIT_FAILURE;
     }
-    // TODO: Add command line arguments to select which algorithm to use
-    // TODO: Add command line argument to select how many words to print
+    int active_algorithms = 0 ;
+    char **filenames = array_new(char *);
+    assert(filenames != NULL);
+    size_t number_of_words = 10;
+    // First, process all arguments, and append all filenames to a list, for later processing
     for (int i = 1; i < argc; i++) {
-        const char *const filename = argv[i];
-        for (size_t j = 0; j < STATIC_ARRAY_LEN(algorithms); j++) {
-            if (process_file(filename, algorithms[j], 10)) {
+        const char *const arg = argv[i];
+        if (arg[0] != '-') {
+            array_push(filenames, (char *)arg);
+            continue;
+        }
+        if (strlen(arg) != 2) {
+            fprintf(stderr, "Error: Unrecognized argument: %s\n", arg);
+            usage(stderr, program_name);
+            return EXIT_FAILURE;
+        }
+        // Check if it was an algorithm option
+        size_t found_algorithm = algorithm_option(arg[1]);
+        if (found_algorithm < STATIC_ARRAY_LEN(algorithms)) {
+            SET_BIT(active_algorithms, found_algorithm);
+            continue;
+        }
+        // Check other argument options
+        switch (arg[1]) {
+        case 'n': {
+            if (++i == argc) {
+                fprintf(stderr, "Error: Argument %s should be followed by a integer number\n", arg);
+                usage(stderr, program_name);
                 return EXIT_FAILURE;
             }
+            if (parse_uint(argv[i], &number_of_words) == EXIT_FAILURE) {
+                fprintf(stderr, "Error: %s is not a valid integer number\n", argv[i]);
+                usage(stderr, program_name);
+                return EXIT_FAILURE;
+            }
+        } break;
+        case 'h':
+            usage(stdout, program_name);
+            return EXIT_SUCCESS;
+        default:
+            fprintf(stderr, "Error: Unrecognized argument: %s\n", arg);
+            usage(stderr, program_name);
+            return EXIT_FAILURE;
         }
     }
+    // If no algorithm was specified, enable all of them
+    if (active_algorithms == 0) {
+        active_algorithms = 0xFF;
+    }
+    if (array_size(filenames) == 0) {
+        fprintf(stderr, "Error: No file was specified...\n");
+        usage(stderr, program_name);
+        return EXIT_FAILURE;
+    }
+    // Process each file using the specified algorithms
+    for (size_t i = 0; i < array_size(filenames); i++) {
+        int print_header = 1;
+        const char *const filename = filenames[i];
+        for (size_t j = 0; j < STATIC_ARRAY_LEN(algorithms); j++) {
+            if (!TEST_BIT(active_algorithms, j)) {
+                continue;
+            }
+            if (process_file(filename, algorithms[j], print_header, number_of_words)) {
+                // The error was already reported in process_file
+                return EXIT_FAILURE;
+            }
+            print_header = 0;
+        }
+    }
+    array_delete(filenames);
     return EXIT_SUCCESS;
 }
